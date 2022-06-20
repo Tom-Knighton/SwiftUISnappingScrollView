4f2e32969887b23326b4c3b2bc753c92697f01db
Tom K <tomknighton@icloud.com>
2022-06-20 11:18:39 +0100

    tweak: make new binding optional in init

--- Sources/SwiftUISnappingScrollView/API/SnappingScrollView.swift
+++ Sources/SwiftUISnappingScrollView/API/SnappingScrollView.swift
@@ -1,15 +1,15 @@
 /**
-*  SwiftUISnappingScrollView
-*  Copyright (c) Ciaran O'Brien 2022
-*  MIT license, see LICENSE file for details
-*/
+ *  SwiftUISnappingScrollView
+ *  Copyright (c) Ciaran O'Brien 2022
+ *  MIT license, see LICENSE file for details
+ */
 
 import SwiftUI
 
 public struct SnappingScrollView<Content, Id>: View
 where Content : View, Id: Hashable
 {
- 
+    
     @Binding var selection: Id
     
     public var body: some View {
@@ -49,8 +49,8 @@ where Content : View, Id: Hashable
                         }
                         .onUpdate(of: geometry.frame(in: .global)) { frame = $0 }
                 }
-                .ignoresSafeArea()
-                .hidden()
+                    .ignoresSafeArea()
+                    .hidden()
             )
             .onChange(of: self.selection) { newValue in
                 withAnimation {
@@ -58,7 +58,7 @@ where Content : View, Id: Hashable
                 }
             }
         }
-       
+        
     }
     
     @StateObject private var delegate = SnappingScrollViewDelegate()
@@ -86,6 +86,10 @@ public extension SnappingScrollView {
     ///     view displays the scrollable component of the content offset, in a way
     ///     suitable for the platform. The default value for this parameter is
     ///     `true`.
+    ///   - selectedId: A value conforming to the Hashable protocol representing
+    ///     the currently centered id.
+    ///     The `content` of the view can be assigned individual ids using `.id(...)`
+    ///     and when changed from the parent view, the SnappingScrollView will scroll to the new id
     ///   - content: The view builder that creates the scrollable view.
     init(_ axis: Axis = .vertical,
          decelerationRate: ScrollDecelerationRate = .normal,
@@ -98,5 +102,31 @@ public extension SnappingScrollView {
         self.decelerationRate = decelerationRate
         self.showsIndicators = showsIndicators
         self._selection = selectedId
+    }
+    
+    /// Creates a new instance that's scrollable in the direction of the given
+    /// axis and can show indicators while scrolling.
+    ///
+    /// - Parameters:
+    ///   - axis: The scroll view's scrollable axis. The default axis is the
+    ///     vertical axis.
+    ///   - decelerationRate: A floating-point value that determines the rate
+    ///     of deceleration after the user ends dragging. The default value for this
+    ///     parameter is `.normal`.
+    ///   - showsIndicators: A Boolean value that indicates whether the scroll
+    ///     view displays the scrollable component of the content offset, in a way
+    ///     suitable for the platform. The default value for this parameter is
+    ///     `true`.
+    ///   - content: The view builder that creates the scrollable view.
+    init(_ axis: Axis = .vertical,
+         decelerationRate: ScrollDecelerationRate = .normal,
+         showsIndicators: Bool = true,
+         @ViewBuilder content: @escaping () -> Content)
+    {
+        self.axis = axis
+        self.content = content
+        self.decelerationRate = decelerationRate
+        self.showsIndicators = showsIndicators
+        self._selection = .constant(0 as! Id)
     }
 }
