7364335deb09b191dd4d0bb71e41f90c8dc4d397
Tom K <tomknighton@icloud.com>
2022-06-20 11:06:05 +0100

    feat: Add binding selection and programmatic scrolling

--- Sources/SwiftUISnappingScrollView/API/SnappingScrollView.swift
+++ Sources/SwiftUISnappingScrollView/API/SnappingScrollView.swift
@@ -6,48 +6,57 @@
 
 import SwiftUI
 
-public struct SnappingScrollView<Content>: View
-where Content : View
+public struct SnappingScrollView<Content, Id>: View
+where Content : View, Id: Hashable
 {
+ 
+    @Binding var selection: Id
+    
     public var body: some View {
-        ScrollView(axis.set, showsIndicators: showsIndicators) {
-            Group {
-                switch axis {
-                case .horizontal:
-                    HStack(content: content)
-                case .vertical:
-                    VStack(content: content)
+        ScrollViewReader { reader in
+            ScrollView(axis.set, showsIndicators: showsIndicators) {
+                Group {
+                    switch axis {
+                    case .horizontal:
+                        HStack(content: content)
+                    case .vertical:
+                        VStack(content: content)
+                    }
                 }
+                .environment(\.scrollViewFrame, frame)
+                .backgroundPreferenceValue(AnchorsKey.self) { anchors in
+                    GeometryReader { geometry in
+                        let frames = anchors.map { geometry[$0] }
+                        
+                        Color.clear
+                            .onAppear { delegate.frames = frames }
+                            .onUpdate(of: frames) { delegate.frames = $0 }
+                    }
+                    .hidden()
+                }
+                .transformPreference(AnchorsKey.self) { $0 = AnchorsKey.defaultValue }
+                .background(UIScrollViewBridge(decelerationRate: decelerationRate.rate, delegate: delegate))
             }
-            .environment(\.scrollViewFrame, frame)
-            .backgroundPreferenceValue(AnchorsKey.self) { anchors in
+            .background(
                 GeometryReader { geometry in
-                    let frames = anchors.map { geometry[$0] }
-                    
                     Color.clear
-                        .onAppear { delegate.frames = frames }
-                        .onUpdate(of: frames) { delegate.frames = $0 }
-                }
-                .hidden()
-            }
-            .transformPreference(AnchorsKey.self) { $0 = AnchorsKey.defaultValue }
-            .background(UIScrollViewBridge(decelerationRate: decelerationRate.rate, delegate: delegate))
-        }
-        .background(
-            GeometryReader { geometry in
-                Color.clear
-                    .onAppear {
-                        DispatchQueue.main.async {
-                            if frame == nil {
-                                frame = geometry.frame(in: .global)
+                        .onAppear {
+                            DispatchQueue.main.async {
+                                if frame == nil {
+                                    frame = geometry.frame(in: .global)
+                                }
                             }
                         }
-                    }
-                    .onUpdate(of: geometry.frame(in: .global)) { frame = $0 }
+                        .onUpdate(of: geometry.frame(in: .global)) { frame = $0 }
+                }
+                .ignoresSafeArea()
+                .hidden()
+            )
+            .onChange(of: self.selection) { newValue in
+                reader.scrollTo(selection, anchor: .center)
             }
-            .ignoresSafeArea()
-            .hidden()
-        )
+        }
+       
     }
     
     @StateObject private var delegate = SnappingScrollViewDelegate()
@@ -79,11 +88,13 @@ public extension SnappingScrollView {
     init(_ axis: Axis = .vertical,
          decelerationRate: ScrollDecelerationRate = .normal,
          showsIndicators: Bool = true,
+         selectedId: Binding<Id>,
          @ViewBuilder content: @escaping () -> Content)
     {
         self.axis = axis
         self.content = content
         self.decelerationRate = decelerationRate
         self.showsIndicators = showsIndicators
+        self._selection = selectedId
     }
 }
